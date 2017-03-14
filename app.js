07dc847c111245b87f476561eda6aac0a91076a4
SamKirkiles <sam.kirkiles@gmail.com>
2017-03-13 23:17:43 -0400

    added signup

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var path = require("path")
 var bodyParser = require('body-parser');
 var messenger = require(__dirname + "/routes/messenger.js");
 var plaid = require(__dirname + "/routes/plaid.js");
@@ -14,13 +15,17 @@ app.use(messenger);
 app.use(plaid);
 
 app.set('views', __dirname + '/views');
-
+app.use("/static", express.static(path.join(__dirname, 'public')))
 
 app.get("/",function(req,res){
-    res.sendFile(__dirname + "/views/index.html")
-    
-    console.log("Hello world")
+  res.sendFile(__dirname + "/views/index.html")
+
+  console.log("Hello world")
 })
+
+app.get("/signup",function(req,res){
+  res.sendFile(__dirname + "/views/signup.html")
+});
 
 app.listen(process.env.PORT);
 
--- routes/plaid.js
+++ routes/plaid.js
@@ -2,32 +2,26 @@ var express = require("express")
 var router = express.Router();
 var plaid = require('plaid');
 require('dotenv').config()
+var mysql = require('mysql');
 
 
-var plaidClient = new plaid.Client(process.env.PLAID_CLIENT_ID,process.env.PLAID_SECRET,plaid.environments.tartan);
+var plaidClient = new plaid.Client(
+    process.env.PLAID_CLIENT_ID,
+    process.env.PLAID_SECRET,
+    process.env.PLAID_PUBLIC_KEY,
+    plaid.environments.sandbox);
 
-router.post("/authenticate", function(req,res){
-    var publicToken = req.body.public_token
-        
-    plaidClient.exchangeToken(publicToken, function(err,exchangeTokenRes){
+router.post("/authenticate", function(request,response){
+    var publicToken = request.body.public_token
+    console.log(publicToken);
+
+    plaidClient.exchangePublicToken(publicToken, function(err, res){
         if (err != null){
-            //handle error
-            res.send("There was an error: " + err.message )
-        }else{
-
-            var access_token = exchangeTokenRes.access_token;
-
-            //save this acces token to database
-
-            plaidClient.getAuthUser(access_token, function(err, authRes){
-                if (err != null){
-                    res.send("There was an error: " + err.message)
-                } else {
-                    var accounts = authRes.accounts;
-                    res.json({accounts:accounts})
-                }
-            })
+            console.log(err.error_message + err.error_code);
+            return
         }
+        access_token = res.access_token;
+        console.log('Access Token: ' + access_token);
     });
 });
 
