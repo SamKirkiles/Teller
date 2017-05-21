6091f45498e740d6a813539d9140f40498b15b3f
SamKirkiles <sam.kirkiles@gmail.com>
2017-05-21 18:46:03 -0400

    added first component

--- App/teller-app/src/app/app.module.ts
+++ App/teller-app/src/app/app.module.ts
@@ -3,11 +3,11 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './app.component';
+import { HomeComponentComponent } from './home-component/home-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    HomeComponentComponent
   ],
   imports: [
     BrowserModule,
