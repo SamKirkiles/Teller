279025f44541263fa2d66c2c1a480aeafbcf7d28
SamKirkiles <sam.kirkiles@gmail.com>
2017-05-23 23:23:10 -0400

    added signup page

--- App/teller-app/src/app/app.module.ts
+++ App/teller-app/src/app/app.module.ts
@@ -5,16 +5,25 @@ import { HttpModule } from '@angular/http';
 
 import { HomeComponentComponent } from './home-component/home-component.component';
 import { HeaderComponent } from './header/header.component';
+import {RouterModule, Routes} from "@angular/router";
+import { SignUpComponent } from './sign-up/sign-up.component';
+
+const appRoutes: Routes = [
+  {path:'', component:HomeComponentComponent},
+  {path:'signup', component:SignUpComponent}
+];
 
 @NgModule({
   declarations: [
     HomeComponentComponent,
-    HeaderComponent
+    HeaderComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [HeaderComponent]
