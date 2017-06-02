9e659ba991306303b676dff732bce753d51b9c66
SamKirkiles <sam.kirkiles@gmail.com>
2017-06-01 23:23:50 -0400

    added msg

--- scripts/applicationstart.sh
+++ scripts/applicationstart.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+msg() {
+    local message=$1
+    echo $message 1>&2
+}
+
 msg "This is called at applicatoin start"
 
 pm2 start /home/ubuntu/teller-install/Server/app.js
--- scripts/beforeinstall.sh
+++ scripts/beforeinstall.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+msg() {
+    local message=$1
+    echo $message 1>&2
+}
+
+
 msg "This is called at before install"
 
 cd /home/ubuntu/teller-install/Server && npm install
