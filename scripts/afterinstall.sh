8a3bf469d30f4d06cce39aec6a8aa8f5664b6004
SamKirkiles <sam.kirkiles@gmail.com>
2017-06-03 00:12:28 -0400

    app should run

--- scripts/afterinstall.sh
+++ scripts/afterinstall.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-msg() {
-    local message=$1
-    echo $message 1>&2
-}
-
-
-msg "This is called at before install"
-
+echo "running hook: after install.sh"
 cd /home/ubuntu/teller-install/Server && npm install
 
 
--- scripts/applicationstart.sh
+++ scripts/applicationstart.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-msg() {
-    local message=$1
-    echo $message 1>&2
-}
-
-msg "This is called at applicatoin start"
-
+echo "Running Hook: applicationstart.sh"
 cd /home/ubuntu/teller-install/Server/ && pm2 start app.js
 pm2 ls
 exit 0
