e99df031a6d29e1d095ffc84a2e31f9e01467641
SamKirkiles <sam.kirkiles@gmail.com>
2017-06-03 20:44:29 -0400

    commands should rnu now

--- scripts/applicationstart.sh
+++ scripts/applicationstart.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
 echo "Running Hook: applicationstart.sh"
-cd /home/ubuntu/teller-install/Server/
-echo pm2 start app.js
-echo sudo pm2 startup
-echo sudo pm2 save
+cd /home/ubuntu/teller-install/Server/ && echo pm2 start app.js && echo sudo pm2 startup && echo sudo pm2 save
 exit 0
