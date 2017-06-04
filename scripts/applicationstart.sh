f4020f2abcac5bb5d50d2c02a7d3d9643795a35f
SamKirkiles <sam.kirkiles@gmail.com>
2017-06-03 21:18:55 -0400

    changed around perms

--- scripts/applicationstart.sh
+++ scripts/applicationstart.sh
@@ -3,6 +3,6 @@
 echo "Running Hook: applicationstart.sh"
 cd /home/ubuntu/teller-install/Server/
 pm2 start app.js
-sudo pm2 startup
-sudo pm2 save
+sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u ubuntu --hp /home/ubuntu
+pm2 save
 exit 0
