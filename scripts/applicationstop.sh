12a1d3cdd995a0611db31cbb1d8736e50a47080c
SamKirkiles <sam.kirkiles@gmail.com>
2017-06-03 21:23:00 -0400

    maybe these perms work

--- scripts/applicationstop.sh
+++ scripts/applicationstop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Running Hook: applicationstop.sh"
-pm2 kill
+sudo pm2 kill
 sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 unstartup systemd -u ubuntu --hp /home/ubuntu
 pm2 save
 
