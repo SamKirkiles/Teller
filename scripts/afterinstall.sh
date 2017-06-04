8c97ee48868a6250084522baf4afaca2644efb90
SamKirkiles <sam.kirkiles@gmail.com>
2017-06-04 11:20:39 -0400

    build now as sudo

--- scripts/afterinstall.sh
+++ scripts/afterinstall.sh
@@ -2,6 +2,6 @@
 
 echo "running hook: after install.sh"
 cd /home/ubuntu/teller-install/Server && npm install
-cd /home/ubuntu/teller-install/App/teller-app && npm install && ng build
+cd /home/ubuntu/teller-install/App/teller-app && npm install && sudo ng build
 
 exit 0
