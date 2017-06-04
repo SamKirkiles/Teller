fe14a8aa44266233e5ea19b72941b3c3e11f5369
SamKirkiles <sam.kirkiles@gmail.com>
2017-06-04 15:09:33 -0400

    fixed env variables hopefully

--- scripts/afterinstall.sh
+++ scripts/afterinstall.sh
@@ -2,21 +2,6 @@
 
 echo "Running hook: after install.sh"
 
-echo export PLAID_CLIENT_ID="58b19b60bdc6a44288ea2050" >> /etc/profile
-echo export PLAID_PUBLIC_KEY="e839335cc584216e29feff916f1d77" >> /etc/profile
-echo export PLAID_SECRET="b9f6580301de6cf4bac33353f87f4e" >> /etc/profile
-
-echo export DATABASE_USERNAME="nodejsec2user" >> /etc/profile
-echo export DATABASE_PASSWORD="Ew359854$" >> /etc/profile
-echo export DATABASE_HOST="mytestdbinstance.c5wy1ciqweer.us-east-1.rds.amazonaws.com" >> /etc/profile
-echo export DATABASE_PORT="3306" >> /etc/profile
-
-echo export FB_MESSENGER_TOKEN="EAAFhcDC2C1IBANSBiWBoZAZBCic6nTAbQZCeZB2NeK5waC6MiKSYkRSgdc02823OtOMRJZC5dfCTPSPdCdVa0hEZB4CJasmYZAADbgBmVMa80EWZAaUc9UIlV6hFPLZBmGHFdgaWSZAknSkZA13t247A4R6iFZBxtw8oQ62u01Cz5yZBYgQZDZD" >> /etc/profile
-echo export FB_VERIFY_TOKEN="tellerverifytoken" >> /etc/profile
-
-echo export PORT="3000" >> /etc/profile
-
-
 cd /home/ubuntu/teller-install/Server && sudo npm install
 cd /home/ubuntu/teller-install/App/teller-app
 sudo npm install
