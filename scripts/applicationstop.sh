a358b91dc504815d5945cf270c9417d223285c22
SamKirkiles <sam.kirkiles@gmail.com>
2017-06-06 14:47:52 -0400

    added application stop script

--- scripts/applicationstop.sh
+++ scripts/applicationstop.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+cd /home/ubuntu/teller-install/Server/
 echo "Running Hook: applicationstop.sh"
 pm2 kill
 sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 unstartup systemd -u ubuntu --hp /home/ubuntu
