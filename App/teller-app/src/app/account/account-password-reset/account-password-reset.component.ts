a458efad39bb618c2a7b4a35525671761ccc28a2
SamKirkiles <sam.kirkiles@gmail.com>
2017-06-21 21:48:55 -0400

    added account password rest

---
+++ App/teller-app/src/app/account/account-password-reset/account-password-reset.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-account-password-reset',
+  templateUrl: './account-password-reset.component.html',
+  styleUrls: ['./account-password-reset.component.css']
+})
+export class AccountPasswordResetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
