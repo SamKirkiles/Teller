eacc48261aa8396118ae1f90c5f03d2d794bc758
SamKirkiles <sam.kirkiles@gmail.com>
2017-06-29 00:57:55 +0200

    ready for deployment

--- scripts/afterinstall.sh
+++ scripts/afterinstall.sh
@@ -5,7 +5,7 @@ echo "Running hook: after install.sh"
 cd /home/ubuntu/teller-install/Server && sudo npm install
 cd /home/ubuntu/teller-install/App/teller-app
 sudo npm install
-sudo ng build
+sudo ng build --prod --env-prod
 
 echo "Finished running hook: after install.sh"
 
