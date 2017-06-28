f3809eac9a22b2cab8d0c951ebbec400e1308586
SamKirkiles <sam.kirkiles@gmail.com>
2017-06-29 01:13:44 +0200

    fixed environemtn issue with afterinstall

--- scripts/afterinstall.sh
+++ scripts/afterinstall.sh
@@ -5,7 +5,7 @@ echo "Running hook: after install.sh"
 cd /home/ubuntu/teller-install/Server && sudo npm install
 cd /home/ubuntu/teller-install/App/teller-app
 sudo npm install
-sudo ng build --prod --env-prod
+sudo ng build --prod --env=rod
 
 echo "Finished running hook: after install.sh"
 
