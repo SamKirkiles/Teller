1a83e486a55eaf90afe8661ace395d169a3dda72
SamKirkiles <sam.kirkiles@gmail.com>
2017-07-29 16:24:04 -0400

    fixed aot bug

--- scripts/afterinstall.sh
+++ scripts/afterinstall.sh
@@ -3,7 +3,7 @@
 echo "Running hook: after install.sh"
 
 cd /home/ubuntu/teller-install/Server && sudo npm install
-cd /home/ubuntu/teller-install/App/teller-app && sudo npm install && sudo ng build --prod --env=prod
+cd /home/ubuntu/teller-install/App/teller-app && sudo npm install && sudo ng build --prod --env=prod --aot=false
 
 echo "Finished running hook: after install.sh"
 
